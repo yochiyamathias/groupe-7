40944bfe04174c914d9266cf8812383877e3b5ea
yochiyamathias <yochiyajeremie@gmail.com>
2024-12-30 12:35:29 +0000

    lundi 30 12h35

--- miniprojet.c
+++ miniprojet.c
@@ -1,6 +1,31 @@
-Gestion des clients hope
+#include <stdio.h>
+#include <string.h>
 
- gestion de compte mathias
-	
- gestion des operations top
+// structure pour un client
+typedef struct {
+    int codecli;
+    char prenom[40];
+    char nom[40];
+    char tel[15];
+    char ville[40];
+} Client;
+
+// structure pour un client
+typedef struct {
+    int numero;
+    char dateOuverture[15];
+    char agence[40];
+    float solde;
+    int codecli; // Lien avec un client
+} Compte;
+
+// structure pour une operatiuon
+typedef struct {
+    int code;
+    int numeroCompte;
+    char type[10]; // "depot" ou "retrait"
+    float montant;
+    char dateOp[15];
+} Operation;
+
 	
